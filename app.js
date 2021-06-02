ab2670d7b46e5abc6c150e50a47516dd9dcbb1a6
santi343 <santiago1988quispep@gmail.com>
2021-06-02 17:29:06 -0300

    tp express-Heroes iniciando

---
+++ app.js
@@ -0,0 +1,49 @@
+const express= require(`express`);
+const path=require(`path`);
+const app=express();
+app.use(express.static('DH-Heroes/public'));
+
+const puerto=3030;
+const views=path.join(__dirname, 'views');
+
+app.listen(puerto, ()=>
+    console.log(`Servidor corriendo en el pueto ${puerto}`)
+)
+
+
+
+app.get('/',(req,res)=>
+    res.sendFile(path.join(views, 'index.html'))
+);
+    
+app.get('/babbage', (req, res)=>
+    res.sendFile(path.join(views, 'babbage.html'))
+);
+
+app.get('/berners-lee', (req, res)=>
+    res.sendFile(path.join(views, 'berners-lee.html'))
+);
+
+app.get('/clarke', (req, res)=>
+    res.sendFile(path.join(views, 'clarke.html'))
+);
+
+app.get('/hamilton', (req,res)=>
+    res.sendFile(path.join(views, 'hamilton.html'))
+);
+
+app.get('/hopper', (req, res)=>
+    res.sendFile(path.join(views, 'hopper.html'))
+);
+
+app.get('/lovelace', (req, res)=>
+    res.sendFile(path.join(views, 'lovelace.html'))
+);
+
+
+app.get('/turing', (req, res)=>
+    res.sendFile(path.join(views, 'turing.html'))
+);
+
+
+
